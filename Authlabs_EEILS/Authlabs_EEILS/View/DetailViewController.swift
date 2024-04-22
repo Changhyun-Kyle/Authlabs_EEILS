ae511b4cb2f07091ddc33034f540d3f7eb2be5bc
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-22 19:51:48 +0900

    feat: DetailViewController 구현

--- Authlabs_EEILS/Authlabs_EEILS/View/DetailViewController.swift
+++ Authlabs_EEILS/Authlabs_EEILS/View/DetailViewController.swift
@@ -5,4 +5,40 @@
 //  Created by 강창현 on 4/22/24.
 //
 
-import Foundation
+import UIKit
+
+final class DetailViewController: UIViewController {
+  private var nameLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    label.text = "test"
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  override func loadView() {
+    super.loadView()
+    configureUI()
+    setupConstraints()
+  }
+  
+  func setupNameLabel(name: String) {
+    self.nameLabel.text = name
+  }
+}
+
+private extension DetailViewController {
+  func configureUI() {
+    view.addSubview(nameLabel)
+    view.backgroundColor = .white
+  }
+  
+  func setupConstraints() {
+    NSLayoutConstraint.activate(
+      [
+        nameLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        nameLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+      ]
+    )
+  }
+}
