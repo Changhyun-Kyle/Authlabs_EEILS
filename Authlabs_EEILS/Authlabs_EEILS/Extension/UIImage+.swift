e43eccff8f7727009716de8266301e81310b9ff6
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-23 20:34:20 +0900

    feat: 이미지 잘라내기 기능 구현

--- Authlabs_EEILS/Authlabs_EEILS/Extension/UIImage+.swift
+++ Authlabs_EEILS/Authlabs_EEILS/Extension/UIImage+.swift
@@ -5,4 +5,28 @@
 //  Created by 강창현 on 4/23/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+  func crop(to rect: CGRect) -> UIImage? {
+    guard
+      let cgImage = self.cgImage
+    else {
+      return nil
+    }
+    
+    let rect = CGRect(
+      x: rect.minX,
+      y: rect.minY,
+      width: rect.width,
+      height: rect.height
+    )
+    
+    guard
+      let croppedImage = cgImage.cropping(to: rect)
+    else {
+      return nil
+    }
+    return UIImage(cgImage: croppedImage)
+  }
+}
