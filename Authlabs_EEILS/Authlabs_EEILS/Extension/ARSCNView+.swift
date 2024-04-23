7fe96a5d46724dadb14be9d38c43084e6e2af340
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-23 20:33:31 +0900

    chore: ARSCNView 및 UIImage extension 파일 추가

---
+++ Authlabs_EEILS/Authlabs_EEILS/Extension/ARSCNView+.swift
@@ -0,0 +1,8 @@
+//
+//  ARSCNView+.swift
+//  Authlabs_EEILS
+//
+//  Created by 강창현 on 4/23/24.
+//
+
+import Foundation
---
+++ Authlabs_EEILS/Authlabs_EEILS/Extension/UIImage+.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+.swift
+//  Authlabs_EEILS
+//
+//  Created by 강창현 on 4/23/24.
+//
+
+import Foundation
