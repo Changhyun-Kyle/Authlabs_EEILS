f1de91dadc1e75b55214e54d7078d7353935e763
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-25 21:18:26 +0900

    feat: Lottie를 활용한 로딩 애니메이션 구현

--- Authlabs_EEILS/Authlabs_EEILS/View/LoadingIndicatorView.swift
+++ Authlabs_EEILS/Authlabs_EEILS/View/LoadingIndicatorView.swift
@@ -5,4 +5,33 @@
 //  Created by 강창현 on 4/25/24.
 //
 
-import Foundation
+import UIKit
+import Lottie
+
+enum LoadingIndicatorView {
+  static func showLoading(in view: UIView) {
+    let animationView: LottieAnimationView
+    
+    guard
+      let existedView = view.subviews.first(where: { $0 is LottieAnimationView } ) as? LottieAnimationView
+    else {
+      animationView = .init(name: "Animation - 1714042188329")
+      animationView.frame = view.bounds
+      animationView.contentMode = .scaleAspectFit
+      animationView.loopMode = .loop
+      animationView.animationSpeed = 0.5
+      animationView.backgroundColor = UIColor(white: 0.2, alpha: 0.9)
+      view.addSubview(animationView)
+      animationView.play()
+      return
+    }
+    animationView = existedView
+    animationView.play()
+  }
+  
+  static func hideLoading(in view: UIView) {
+    DispatchQueue.main.async {
+      view.subviews.filter({ $0 is LottieAnimationView }).forEach { $0.removeFromSuperview() }
+    }
+  }
+}
