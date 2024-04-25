98e8836e2b2ae7e719b2233e6f1c53b7ac049210
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-25 21:17:51 +0900

    feat: DetailView 구현

--- Authlabs_EEILS/Authlabs_EEILS/View/ContentStackView.swift
+++ Authlabs_EEILS/Authlabs_EEILS/View/ContentStackView.swift
@@ -5,4 +5,105 @@
 //  Created by 강창현 on 4/25/24.
 //
 
-import Foundation
+import UIKit
+
+final class ContentStackView: UIStackView {
+  private var definitionTitle: UILabel = {
+    let label = UILabel()
+    label.text = "정의"
+    label.font = .preferredFont(for: .body, weight: .bold)
+    label.textColor = .black
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  private var definitionLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    label.numberOfLines = 0
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  private lazy var definitionStackView: UIStackView = {
+    let stackView = UIStackView(
+      arrangedSubviews: [
+        definitionTitle,
+        definitionLabel
+      ]
+    )
+    stackView.axis = .vertical
+    stackView.distribution = .fill
+    stackView.alignment = .leading
+    stackView.spacing = 3
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  private var descriptionTitle: UILabel = {
+    let label = UILabel()
+    label.text = "설명"
+    label.font = .preferredFont(for: .body, weight: .bold)
+    label.textColor = .black
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  private var descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    label.numberOfLines = 0
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  private lazy var descriptionStackView: UIStackView = {
+    let stackView = UIStackView(
+      arrangedSubviews: [
+        descriptionTitle,
+        descriptionLabel
+      ]
+    )
+    stackView.axis = .vertical
+    stackView.distribution = .fill
+    stackView.alignment = .leading
+    stackView.spacing = 3
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    return stackView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(definition: String, description: String) {
+    definitionLabel.text = definition
+    descriptionLabel.text = description
+  }
+}
+
+private extension ContentStackView {
+  func configureUI() {
+    self.axis = .vertical
+    self.spacing = 5
+    self.distribution = .fill
+    self.addArrangedSubview(definitionStackView)
+    self.addArrangedSubview(descriptionStackView)
+  }
+  
+  func setupConstraints() {
+    NSLayoutConstraint.activate(
+      [
+        definitionStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+        definitionStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+        descriptionStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+        descriptionStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+      ]
+    )
+  }
+}
--- Authlabs_EEILS/Authlabs_EEILS/View/DetailViewController.swift
+++ Authlabs_EEILS/Authlabs_EEILS/View/DetailViewController.swift
@@ -8,93 +8,24 @@
 import UIKit
 
 final class DetailViewController: UIViewController {
-  
-  private var titleLabel: UILabel = {
-    let label = UILabel()
-    label.text = "분석결과"
-    label.textAlignment = .left
-    label.textColor = .black
-    label.font = .preferredFont(forTextStyle: .largeTitle)
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  private var nameLabel: UILabel = {
-    let label = UILabel()
-    label.textColor = .black
-    label.numberOfLines = 0
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  private var captureImageView: UIImageView = {
-    let imageView = UIImageView()
-    imageView.contentMode = .scaleAspectFit
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    return imageView
-  }()
-  
-  private var originalImageView: UIImageView = {
-    let imageView = UIImageView()
-    imageView.contentMode = .scaleAspectFit
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    return imageView
-  }()
-  
-  private lazy var imageStackView: UIStackView = {
-    let stackView = UIStackView(
-      arrangedSubviews:
-        [
-          originalImageView,
-          captureImageView
-        ]
-    )
-    stackView.axis = .horizontal
-    stackView.distribution = .fillEqually
-    stackView.spacing = 5
-    stackView.alignment = .center
-    stackView.alignment = .top
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    return stackView
-  }()
+  private let detailView = DetailView()
   
   override func loadView() {
     super.loadView()
-    configureUI()
-    setupConstraints()
+    view = detailView
   }
   
   func configure(
-    name: String,
+    definition: String,
+    description: String,
     captureImage: UIImage,
     originalImage: UIImage
   ) {
-    self.nameLabel.text = name
-    self.captureImageView.image = captureImage
-    self.originalImageView.image = originalImage
-  }
-}
-
-private extension DetailViewController {
-  func configureUI() {
-    view.addSubview(nameLabel)
-    view.addSubview(imageStackView)
-    view.addSubview(titleLabel)
-    view.backgroundColor = .white
-  }
-  
-  func setupConstraints() {
-    NSLayoutConstraint.activate(
-      [
-        titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
-        nameLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 10),
-        nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
-        nameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
-        imageStackView.topAnchor.constraint(equalTo: self.nameLabel.bottomAnchor, constant: 10),
-        imageStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
-        imageStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
-        imageStackView.heightAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.height * 0.3),
-      ]
+    detailView.configure(
+      definition: definition,
+      description: description,
+      captureImage: captureImage,
+      originalImage: originalImage
     )
   }
 }
