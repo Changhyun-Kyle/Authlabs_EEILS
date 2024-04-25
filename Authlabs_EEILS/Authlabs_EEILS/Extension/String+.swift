30b0f5b33faa3d0e8be443a6fc2de831c33f4a8e
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-25 21:19:10 +0900

    feat: 응답 받은 Response를 정의와 설명으로 나눠주는 formatResponse() 구현

--- Authlabs_EEILS/Authlabs_EEILS/Extension/String+.swift
+++ Authlabs_EEILS/Authlabs_EEILS/Extension/String+.swift
@@ -5,4 +5,27 @@
 //  Created by 강창현 on 4/25/24.
 //
 
-import Foundation
+extension String {
+  func formatResponse() -> (definition: String, description: String) {
+    let components = self.components(separatedBy: "\n")
+    var definition: String?
+    var description: String?
+    
+    for component in components {
+      if component.hasPrefix("정의:") {
+        definition = component.replacingOccurrences(of: "정의: ", with: "")
+      } else if component.hasPrefix("설명:") {
+        description = component.replacingOccurrences(of: "설명: ", with: "")
+      }
+    }
+    
+    guard
+      let definition = definition,
+      let description = description
+    else {
+      return ("정의 없음", "설명 없음")
+    }
+    
+    return (definition, description)
+  }
+}
